978bbd2f90f33b1e8dde1852752eb13d24edf844
Fatwah Firmansyah <fatwah091@gmail.com>
2025-03-29 10:10:09 +0700

    adding webhook to sync user data from clerk to convex

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useAuth } from '@clerk/clerk-expo'
 
 export default function Index() {
+  const {signOut} = useAuth()
   return (
     <View>
-      <Text>Home Screen</Text>
+      <TouchableOpacity onPress={()=> signOut()}>
+        <Text>Sign out</Text>
+      </TouchableOpacity>
     </View>
   )
 }
