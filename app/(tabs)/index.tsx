5aff5b89548a7e78bd244795213b943eaf519c92
Fatwah Firmansyah <fatwah091@gmail.com>
2025-03-30 12:51:24 +0700

    Post in home n like functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,64 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
 import { useAuth } from '@clerk/clerk-expo'
+import { styles } from '@/styles/feed.style'
+import { Ionicons } from '@expo/vector-icons'
+import { COLORS } from '@/constants/theme'
+import { STORIES } from '@/constants/mock-data'
+import Story from '@/components/Story'
+import { useQuery } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import Loader from '@/components/loader'
+import Post from '@/components/post'
 
 export default function Index() {
-  const {signOut} = useAuth()
+  const { signOut } = useAuth()
+  const posts = useQuery(api.posts.getFeedPosts)
+  if (posts === undefined) return <Loader />
+  if (posts.length === 0) return <NoPostFound />
   return (
-    <View>
-      <TouchableOpacity onPress={()=> signOut()}>
-        <Text>Sign out</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+
+      {/* HEADER */}
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>spotlight</Text>
+        <TouchableOpacity onPress={() => signOut()}>
+          <Ionicons name='log-out-outline' size={24} color={COLORS.white} />
+        </TouchableOpacity>
+      </View>
+
+      {/* CONTENT */}
+      <ScrollView showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom:60 }}
+      >
+
+        {/* STORIES */}
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={styles.storiesContainer}
+        >
+          {STORIES.map((story) => (<Story key={story.id} story={story} />))}
+        </ScrollView>
+
+          {posts.map((post) => (<Post key={post._id} post={post} />))}
+      </ScrollView>
+
     </View>
   )
 }
+
+const NoPostFound = () => (
+  <View
+    style={
+      {
+        flex: 1,
+        backgroundColor: COLORS.background,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }
+    }
+  >
+    <Text style={{ fontSize: 20, color: COLORS.primary }}>No posts yet</Text>
+  </View>
+)
---
+++ constants/mock-data.ts
@@ -0,0 +1,20 @@
+export const STORIES = [
+    {
+        id: "1",
+        username: "You",
+        avatar: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=400&h=400&fit=crop",
+        hasStory: false,
+    },
+    {
+        id: "2",
+        username: "sarah.k",
+        avatar: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400&h=400&fit=crop",
+        hasStory: true,
+    },
+    {
+        id: "3",
+        username: "alex.m",
+        avatar: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=400&h=400&fit=crop",
+        hasStory: true,
+    },
+];
